b97f25c39e55c19ce1bc6c531de68e972c164570
sunshine-web-coder <webdev4gaming@gmail.com>
2024-04-24 15:43:05 +0100

    product server section updated

--- js/tabs.js
+++ js/tabs.js
@@ -5,16 +5,28 @@ function openTab(tabId) {
   for (var i = 0; i < tabContents.length; i++) {
     tabContents[i].style.display = "none";
   }
-
+  
   // Remove 'active' class from all tab links
   var tabLinks = document.getElementsByClassName("tablink");
   for (var i = 0; i < tabLinks.length; i++) {
     tabLinks[i].classList.remove("active");
   }
-
+  
   // Show the selected tab content
   document.getElementById(tabId).style.display = "block";
-
+  
   // Add 'active' class to the clicked tab link
-  event.currentTarget.classList.add("active");
+  var activeTabLink = document.querySelector('[onclick="openTab(\'' + tabId + '\')"]');
+  if (activeTabLink) {
+    activeTabLink.classList.add("active");
+  }
+}
+
+// Function to scroll to the tab container and toggle the tab
+function scrollToTab(tabId) {
+  var tabContainer = document.getElementById(tabId);
+  if (tabContainer) {
+    tabContainer.scrollIntoView({ behavior: "smooth" });
+    openTab(tabId);
+  }
 }
